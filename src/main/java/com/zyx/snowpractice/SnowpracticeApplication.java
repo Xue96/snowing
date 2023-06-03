942218c65f0a38e4e91840daff9b79002b9977b1
Xue96 <zhangyuxue_job@163.com>
2023-06-03 17:18:55 +0800

    将snowpractice项目添加到本地仓库中

---
+++ src/main/java/com/zyx/snowpractice/SnowpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.zyx.snowpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowpracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyx/snowpractice/SnowpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.snowpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
